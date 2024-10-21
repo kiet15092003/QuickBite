9696ac7d321554a02059d15d3b1b5f8860142b34
kygoree1509 <kiet.kieu.cao.minh@preciofishbone.se>
2024-10-21 17:29:19 +0700

    init header page

---
+++ clients/user-ui/src/screens/HomeScreen.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../components/Header'
+
+const HomeScreen = () => {
+  return (
+    <div>
+      <div className='h-10'>
+      <Header/>
+    </div>
+    <div className='h-90'> asdnakjsdna </div>
+    </div>
+  )
+}
+
+export default HomeScreen
