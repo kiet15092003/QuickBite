8afa192e7e5a36aa9401ef250eabfbfdabba726e
kiet15092003 <kieucaominhkiet15092003@gmail.com>
2024-10-30 12:03:43 +0700

    fix prisma user address

--- apps/apps/api-users/src/email/email.module.ts
+++ apps/apps/api-users/src/email/email.module.ts
@@ -23,7 +23,7 @@ import { EjsAdapter } from '@nestjs-modules/mailer/dist/adapters/ejs.adapter';
           from: 'QuickBite',
         },
         template: {
-          dir: join(__dirname, '../../../../servers/email-templates'),
+          dir: join(__dirname, '../../../apps/api-users/email-templates'),
           adapter: new EjsAdapter(),
           options: {
             strict: false,
